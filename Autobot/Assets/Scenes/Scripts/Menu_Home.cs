eb24d840f6c1b588d8214237ad0d5ab621739b75
Finalor <iflox974@gmail.com>
2023-01-28 18:36:55 +0100

    Essaie pour la scène Mentions_Légale

--- Autobot/Assets/Scenes/Scripts/Menu_Home.cs
+++ Autobot/Assets/Scenes/Scripts/Menu_Home.cs
@@ -1,15 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Menu_Home : MonoBehaviour
 {
 
-    // Camera donnée par Unity
-    public new Camera camera;
-
-    // Vitesse de la caméra (Duration de la translation d'un point A vers un point B)
-    public float duration = 0.05f;
 
     // Start is called before the first frame update
     void Start()
@@ -23,46 +19,19 @@ public class Menu_Home : MonoBehaviour
 
     }
 
-    // Fonction appelée lors du clic sur le bouton "Mention Légales", on translate la caméra vers la droite de 275.
+    // Fonction qui permet de load la scène "Mentions_Légale"
     public void OnClickMentionsLegales()
     {
-        // On démarre une coroutine qui va se charger de déplacer la caméra vers la position cible
-        StartCoroutine(MoveCamera(camera.transform.position + new Vector3(275, 0, 0), duration));
-        // on affiche dans la console Unity le message "Mentions Légales"
-        Debug.Log("Mentions Légales");
+        Debug.Log("Mentions Légale");
+        SceneManager.LoadScene("Mentions_Légale");
     }
 
-    // Fonction qui vas faire le mouvement de la caméra fluide qui prend en paramètre la destination et la durée de la transition
-    IEnumerator MoveCamera(Vector3 targetPosition, float duration)
-    {
-        // On récupère la position initiale de la caméra
-        Vector3 initialPosition = camera.transform.position;
+   
 
-        // On initialise le temps écoulé à 0
-        float elapsedTime = 0;
-
-        // Tant que le temps écoulé est inférieur à la durée de la transition
-        while (elapsedTime < duration)
-        {
-            // On utilise la fonction "Lerp" pour effectuer une transition fluide vers la position cible
-            camera.transform.position = Vector3.Lerp(initialPosition, targetPosition, elapsedTime / duration);
-
-            // On augmente le temps écoulé de Time.deltaTime (temps écoulé depuis la dernière frame)
-            elapsedTime += Time.deltaTime;
-
-            // On attend jusqu'à la prochaine frame avant de continuer la boucle
-            yield return null;
-        }
-        // On s'assure que la caméra arrive à la position cible
-        camera.transform.position = targetPosition;
-    }
-
-    // Fonction appelée lors du clic sur le bouton "Retour", on translate la caméra vers la gauche de 275.
+    // Fonction qui permet de load la scène "Accueil"
     public void OnClickRetourMentions()
     {
-        // On démarre une coroutine qui va se charger de déplacer la caméra vers la position cible
-        StartCoroutine(MoveCamera(camera.transform.position + new Vector3(-275, 0, 0), duration));
-        // on affiche dans la console Unity le message "Retour"
-        Debug.Log("Retour");
+        Debug.Log("Accueil");
+        SceneManager.LoadScene("Accueil");
     }
 }
