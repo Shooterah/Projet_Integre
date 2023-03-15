c9896bd5a0e65d41a5b042a0a22aa76afe39be3d
jeoffrey42 <jeoffrey.pereira@live.fr>
2023-03-15 10:56:05 +0100

    peaufinage meteo , ajout inclinaison , ajout largeur

--- Autobot/Assets/Scripts/Modif_Env.cs
+++ Autobot/Assets/Scripts/Modif_Env.cs
@@ -6,6 +6,7 @@ using TMPro;
 
 public class Modif_Env : MonoBehaviour
 {
+    [SerializeField] GameObject instance;
     [SerializeField] GameObject prefab;
     [SerializeField] GameObject particle;
     [SerializeField] TMP_Dropdown Meteo;
@@ -22,25 +23,13 @@ public class Modif_Env : MonoBehaviour
             // charger une prefab provenant du fichier scenes prefab
             prefab = Resources.Load("Prefabs/Environement") as GameObject;
             // instancier la prefab avec un tag Véhicule
-            GameObject instance = Instantiate(prefab, new Vector3(-50, -25, 105), Quaternion.identity) as GameObject;
+            instance = Instantiate(prefab, new Vector3(-50, -25, 105), Quaternion.identity) as GameObject;
             // rotation de la prefab -100 sur l'axe x
-            instance.transform.Rotate(-100, 0, 0);
+            instance.transform.Rotate(-90, 100, -100);
             instance.tag = "Environement";
-
-            // delet array element 8 4 3
-            Material[] materials = instance.GetComponent<MeshRenderer>().materials;
-            Material[] newMaterials = new Material[materials.Length - 1];
-            int j = 0;
-            for (int i = 0; i < materials.Length; i++)
-            {
-                if (i != 8)
-                {
-                    newMaterials[j] = materials[i];
-                    j++;
-                }
-            }
-            instance.GetComponent<MeshRenderer>().materials = newMaterials;
-        } 
+            instance.GetComponent<MeshRenderer>().materials = SupprIndesirable(instance);
+            Change_meteo(instance, "Soleil");
+        }
         // inclinaison par défaut
         Inclinaison_route.text = "0";
         // largeur par défaut
@@ -53,49 +42,72 @@ public class Modif_Env : MonoBehaviour
     string etat_route = Etat_route.options[Etat_route.value].text;
     int inclinaison = int.Parse(Inclinaison_route.text);
     int largeur = int.Parse(Largeur_route.text);
-     
+    
+    if (largeur < -200 || largeur > 200 ){
+        Largeur_route.text = "0";
+        largeur = 0;
+        largeur = int.Parse(Largeur_route.text);
+    }
+
+    if (inclinaison <= 0 || inclinaison > 30 ){
+        Inclinaison_route.text = "0";
+        inclinaison = 0;
+        inclinaison = int.Parse(Inclinaison_route.text);
+    }
+
+
+    // delete la prefab particule
     if (GameObject.FindGameObjectsWithTag("Particule").Length != 0){
     GameObject[] Particule = GameObject.FindGameObjectsWithTag("Particule");
-    foreach (GameObject particule in Particule){
-        Destroy(particule);
-    }}
-    // récupération de la prefab environement
-    prefab = GameObject.FindWithTag("Environement");
-    Material[] newprefab = prefab.GetComponent<MeshRenderer>().materials;
-    //fait spawn neige 
-    if (meteo == "Neige"){
+        foreach (GameObject particule in Particule){
+            // si la particule est différente de la particule actuelle
+            Destroy(particule);
+        }
+    }
+    // delete la prefab environement
+    if (GameObject.FindGameObjectsWithTag("Environement").Length != 0){
+    GameObject[] Environement = GameObject.FindGameObjectsWithTag("Environement");
+        foreach (GameObject environement in Environement){
+            Destroy(environement);
+        }
+    }
 
+    // changer les valeurs de la prefab
+    prefab = Resources.Load("Prefabs/Environement") as GameObject;
+    instance = Instantiate(prefab, new Vector3(-50, -25, 105), Quaternion.identity) as GameObject;
+    instance.transform.Rotate(-90-inclinaison, 100, -100);
+    instance.transform.localScale = new Vector3(1000,largeur + 1000, 1000);
+    instance.tag = "Environement";
+    instance.GetComponent<MeshRenderer>().materials = SupprIndesirable(instance);
+    Change_meteo(instance, meteo);
+    }
+
+    //********************************Fonctions*************************************************//
+
+    public Material[] SupprIndesirable(GameObject instance)
+    {
+        Material[] materials = instance.GetComponent<MeshRenderer>().materials;
+        Material[] newMaterials = new Material[materials.Length - 1];
+        int j = 0;
+        for (int i = 0; i < materials.Length; i++)
+        {
+            if (i != 8)
+            {
+                newMaterials[j] = materials[i];
+                j++;
+            }
+        }
+        return newMaterials;
+    }
+    public void Change_meteo(GameObject instance, string meteo){
+        Material[] newprefab = instance.GetComponent<MeshRenderer>().materials;
         particle = Resources.Load("Prefabs/"+meteo) as GameObject;
-        GameObject instance2 = Instantiate(particle, new Vector3(-60, 60, 105), Quaternion.identity) as GameObject;
-        //scale de la particule
-        instance2.transform.localScale = new Vector3(10,10,10);
+        GameObject instance2 = Instantiate(particle, new Vector3(-60, 50, 105), Quaternion.identity) as GameObject;
         instance2.transform.Rotate(90, 0, 0);
         instance2.tag = "Particule";
-
-        newprefab[0] = Resources.Load("Textures/"+meteo) as Material;
-        newprefab[1] = Resources.Load("Textures/"+meteo) as Material;
-        newprefab[2] = Resources.Load("Textures/"+meteo) as Material;
-        prefab.GetComponent<MeshRenderer>().materials = newprefab;
-       }
-    if (meteo == "Soleil"){
-
-        newprefab[0] = Resources.Load("Textures/rock") as Material;
-        newprefab[1] = Resources.Load("Textures/bluegreen") as Material;
-        newprefab[2] = Resources.Load("Textures/yellowgreen") as Material;
-        prefab.GetComponent<MeshRenderer>().materials = newprefab;
-    }
-    if (meteo == "Pluie"){
-        particle = Resources.Load("Prefabs/"+meteo) as GameObject;
-        GameObject instance2 = Instantiate(particle, new Vector3(-60, 60, 105), Quaternion.identity) as GameObject;
-        //scale de la particule
-        instance2.transform.localScale = new Vector3(10,10,10);
-        instance2.transform.Rotate(90, 0, 0);
-        instance2.tag = "Particule";
-
-        newprefab[0] = Resources.Load("Textures/rock_pluie") as Material;
-        newprefab[1] = Resources.Load("Textures/bluegreen") as Material;
-        newprefab[2] = Resources.Load("Textures/yellowgreen_pluie") as Material;
-        prefab.GetComponent<MeshRenderer>().materials = newprefab;
-    }
-    }
+        newprefab[0] = Resources.Load("Textures/rock_"+meteo) as Material;
+        newprefab[1] = Resources.Load("Textures/bluegreen_"+meteo) as Material;
+        newprefab[2] = Resources.Load("Textures/yellowgreen_"+meteo) as Material;
+        instance.GetComponent<MeshRenderer>().materials = newprefab;
+        }
 }
